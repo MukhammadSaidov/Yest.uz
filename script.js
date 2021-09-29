f90ea7a47dbedb98086a7aae433839bfa7bb2216
MukhammadSaidov <77840212+MukhammadSaidov@users.noreply.github.com>
2021-09-29 18:00:04 +0500

    Yest.uz

---
+++ script.js
@@ -0,0 +1,78 @@
+/* Restaurant */
+$('#res').hover(
+    function () {
+        $('.bee_img').attr('style', 'transform:rotate(80deg); transition: 700ms;');
+    },
+    function () {
+    $('.bee_img').attr('style', 'transform:rotate(0); transition: 700ms;');
+});
+
+/* Flower */
+$('#flow').hover(
+    function () {
+        $('.bee_img').attr('style', 'transform:rotate(40deg); transition: 700ms;');
+    },
+    function () {
+    $('.bee_img').attr('style', 'transform:rotate(0); transition: 700ms;');
+});
+
+/* Shop */
+$('#shopping').hover(
+    function () {
+        $('.bee_img').attr('style', 'transform:rotate(-85deg); transition: 700ms;');
+    },
+    function () {
+    $('.bee_img').attr('style', 'transform:rotate(0); transition: 700ms;');
+});
+
+/* Courier */
+$('#cour').hover(
+    function () {
+        $('.bee_img').attr('style', 'transform:rotate(-50deg); transition: 700ms;');
+    },
+    function () {
+    $('.bee_img').attr('style', 'transform:rotate(0); transition: 700ms;');
+});
+
+/* Present */
+$('#present').hover(
+    function () {
+        $('.bee_img').attr('style', 'transform:rotate(20deg); transition: 700ms;');
+    },
+    function () {
+    $('.bee_img').attr('style', 'transform:rotate(0); transition: 700ms;');
+});
+
+/* Shashlik.uz */
+$('#shashlik').hover(
+    function () {
+        $('.bee_img').attr('style', 'transform:rotate(-20deg); transition: 700ms;');
+    },
+    function () {
+    $('.bee_img').attr('style', 'transform:rotate(0); transition: 700ms;');
+});
+
+
+
+const Top = document.querySelector('.Top_icon')
+const Header = document.querySelector('.header')
+
+//OnScroll Offset
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 100) {
+        Top.classList.add('active')
+        Header.classList.add('active')
+    } else {
+        Top.classList.remove('active')
+        Header.classList.remove('active')
+    }
+})
+
+//Top Click
+Top.addEventListener("click", function(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+})
